b87c943f970ea3eff250a1b856123801ce8e6afc
Akhilesh <akhileshpujar796@gmail.com>
2023-11-16 14:49:01 +0530

    first commit

---
+++ src/lib/nextauth.ts
@@ -0,0 +1,71 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { type GetServerSidePropsContext } from "next";
+import {
+  getServerSession,
+  type NextAuthOptions,
+  type DefaultSession,
+} from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+import { prisma } from "@/lib/db";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    jwt: async ({ token }) => {
+      const db_user = await prisma.user.findFirst({
+        where: {
+          email: token?.email,
+        },
+      });
+      if (db_user) {
+        token.id = db_user.id;
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+   Auth0Provider({
+    clientId: process.env.AUTH0_CLIENT_ID as string,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+    issuer: process.env.AUTH0_ISSUER
+  })
+  ],
+};
+
+export const getAuthSession = () => {
+  return getServerSession(authOptions);
+};
